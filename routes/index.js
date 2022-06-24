2f57fb0168a2b6531be18fb3f6f9dd726fcbbe40
Sebastian Laca <seba.laca@gmail.com>
2022-06-24 18:26:53 +0200

    Hasta iteracion 2

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model');
+
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+router.get("/movies", (req, res) => {
+    Movie.find()
+    .then((response) => {
+        console.log(response)
+        res.render("movies.hbs", {response})
+    })
+    .catch((error) => console.log(error));
+})
 module.exports = router;
